df663611329c75b1429224ccc1f4f339f9bfb3bd
fortunedayssss <717622995@qq.com>
2016-09-14 16:08:59 +0800

    914

---
+++ src/main/java/etc/questionSetCreator.java
@@ -0,0 +1,73 @@
+package etc;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by yisic on 2016/9/12.
+ */
+public class questionSetCreator {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        File rFile = new File("src/main/resources/qSet.txt");
+        InputStreamReader read = new InputStreamReader(new FileInputStream(rFile), "GBK");//考虑到编码格式
+        BufferedReader bufferedReader = new BufferedReader(read);
+        String lineTxt;
+
+
+        /*****/
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+
+        Element root = document.createElement("QUESTIONBANK");
+        document.appendChild(root);
+
+
+        /*****/
+
+
+        int counter = 0;
+        while ((lineTxt = bufferedReader.readLine()) != null) {
+            System.out.println(++counter + "/173");
+            System.out.println(lineTxt);
+
+            Element doc = document.createElement("DOC");
+            Element question = document.createElement("Question");
+            question.appendChild(document.createTextNode(lineTxt));
+            doc.appendChild(question);
+            Element questionStyle = document.createElement("QuestionStyle");
+            String style = scanner.next();
+            if (style.equals("exit"))
+                break;
+            questionStyle.appendChild(document.createTextNode(style));
+            doc.appendChild(questionStyle);
+            root.appendChild(doc);
+
+        }
+
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        DOMSource source = new DOMSource(document);
+        transformer.setOutputProperty(OutputKeys.ENCODING, "GBK");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        PrintWriter pw = new PrintWriter(new FileOutputStream("src/main/resources/qSet.xml"));
+        StreamResult result = new StreamResult(pw);
+        transformer.transform(source, result);
+        System.out.println("生成XML文件成功!");
+
+
+    }
+}
---
+++ src/main/java/spider/Spider.java
@@ -0,0 +1,62 @@
+package spider;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by yisic on 2016/8/29.
+ */
+public class Spider {
+    private static String getAnswerFromZhiDaoPage(String url) throws Exception {
+        //只抽取有最佳答案的百度知道问题对
+        Document document = Jsoup.connect(url).get();
+        try {
+            return document.getElementsByClass("best-text").get(0).text();
+        } catch (IndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+
+    public static ArrayList<String> getAnswerFromZhiDao(String question) throws Exception {
+        ArrayList<String> answerSet = new ArrayList<String>();
+        String url = "http://zhidao.baidu.com/search?ie=gbk&word=" + URLEncoder.encode(question, "GBK");
+        Document document = Jsoup.connect(url).get();
+        //最佳匹配
+        String bestZhiDaoQAURL = "";
+        try {
+            bestZhiDaoQAURL = document.getElementsByClass("special-box-p").get(0).getElementsByClass("dt").get(0).getElementsByAttribute("href").get(0).attr("abs:href");
+        } catch (IndexOutOfBoundsException e) {
+            //answerSet.add("无");
+        }
+        if (!bestZhiDaoQAURL.equals(""))
+            answerSet.add(getAnswerFromZhiDaoPage(bestZhiDaoQAURL));
+        //第一页所有非最佳匹配问答对
+        Elements answerURL = document.getElementsByClass("ti");
+        for (Element ansURL : answerURL) {
+            String urlTemp = ansURL.attr("abs:href");
+            String ansTemp;
+            if (!urlTemp.equals("") && !(ansTemp = getAnswerFromZhiDaoPage(urlTemp)).equals(""))
+                answerSet.add(ansTemp);
+        }
+        return answerSet;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String question;
+        ArrayList<String> answerSet;
+        while (!(question = scanner.nextLine()).equals("end")) {
+            answerSet = getAnswerFromZhiDao(question);
+            for (int i = 0; i < answerSet.size(); i++) {
+                System.out.println(i + ":");
+                System.out.println(answerSet.get(i));
+            }
+        }
+    }
+}
