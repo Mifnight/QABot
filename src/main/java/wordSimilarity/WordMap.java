09d7a0e93413b2d8cb8d65b6e1772a4880a4a246
LuyaoLI <286299392@qq.com>
2016-09-16 10:58:42 +0800

    Implement similarity calculating by 同义词词林.(hgd.txt)

---
+++ src/main/java/wordSimilarity/WordMap.java
@@ -0,0 +1,49 @@
+package wordSimilarity;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Luyao-Li on 2016/9/14.
+ */
+public class WordMap {
+    private HashMap<String,ArrayList<String>> wordMaps = new HashMap<String, ArrayList<String>>();
+
+    public WordMap()throws IOException{
+        File file = new File(Object.class.getResource("/hgd.txt").getFile());
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(file),"UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        String str = br.readLine();
+        while(str!=null) {
+            wordMaps.put(str.substring(0,str.indexOf(" ")),WordTree.getWordList(str));
+            str=br.readLine();
+        }
+    }
+
+    public ArrayList<String> getWordNoByValue(String wordValue){
+        ArrayList<String> wordNoList = new ArrayList<String>();
+        for(Map.Entry<String,ArrayList<String>> word:wordMaps.entrySet()){
+            if(word.getValue().contains(wordValue)){
+                wordNoList.add(word.getKey());
+            }
+        }
+        return wordNoList;
+    }
+
+    public int sameFirstWordsNum(String str){
+        System.out.println(str);//
+        int count=0;
+        for(Map.Entry<String,ArrayList<String>> word:wordMaps.entrySet()){
+            if(word.getKey().substring(0,str.length()).equals(str)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args)throws IOException{
+
+    }
+}
